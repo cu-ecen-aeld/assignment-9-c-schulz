6ec817771694a9d00517587902460681fbef9c71
Cornelia Schulz <cornelia.schulz@web.de>
2026-04-16 23:10:50 +0200

    update commit id to modified finder-test.sh

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '2f1ee34e9578f8d193f733c30c5c063b3fd5e336'
+AESD_ASSIGNMENTS_VERSION = 'e17f2895312d59b7183bfd2596770b88a8b0be03'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
