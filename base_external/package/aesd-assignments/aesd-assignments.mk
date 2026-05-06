8ad103a54045ed6a710be3c665e1cc0228a90696
Cornelia Schulz <cornelia.schulz@web.de>
2026-05-06 13:11:06 +0200

    add aesdchar kernel module

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,10 +6,13 @@
 ##############################################################
 
 # assignment 4) & 5) reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '6302c888187504023120f00250c54a3548a4fe57'
+AESD_ASSIGNMENTS_VERSION = 'fbe2734029a3da72072ffb182932a2d107742e71'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-c-schulz.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+# assignment 8) build the aesd-char-driver components for the kernel module
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
@@ -18,17 +21,22 @@ endef
 
 # assignment 4) add your writer, finder and finder-test utilities/scripts to the installation steps below
 # assignment 5) install your aesdsocket executable and your aesdsocket-start-stop script
+# assignment 8) install aesdchar load and unload scripts
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh       			$(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh  			$(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer          			$(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh                   $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh              $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer                      $(TARGET_DIR)/usr/bin/
 
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket						$(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop			$(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket                      $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop           $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
+    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load         $(TARGET_DIR)/usr/bin/
+    $(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload       $(TARGET_DIR)/usr/bin/
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
