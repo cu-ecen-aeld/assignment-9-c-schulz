c225b9a06f398bf85baf009625218e906865a704
Cornelia Schulz <cornelia.schulz@web.de>
2026-05-08 10:44:30 +0200

    update

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 # assignment 4) & 5) & 8) reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '3387d0c23180f7996d89a41d6367fa2d143072bb'
+AESD_ASSIGNMENTS_VERSION = '0b694ed2c14dddab807b9d334cbe9beab29a7c29'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-c-schulz.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
